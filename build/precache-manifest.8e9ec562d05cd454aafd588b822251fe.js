2eb04e6f6f9c95d73b6788752ad89d135135d3ee
program-ui <twq19911@gmail.com>
2021-07-30 20:45:10 +0800

    d

---
+++ build/precache-manifest.8e9ec562d05cd454aafd588b822251fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3283c8dcc811d77a117e596c43e810c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b985c9ca4a1b34b1bd",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b985c9ca4a1b34b1bd",
+    "url": "/static/js/main.9ca74cf8.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
